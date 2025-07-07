6736373c729ee6a994f152448c9998aa740bf48b
Marcelo de Oliveira Lima <marceloliveirali@gmail.com>
2025-07-06 21:43:55 -0300

    product routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+
 function App() 
 {
     return (
         <>
-
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home />} >
+                        <Route index element={<Navigate to="/home" />} />
+                        <Route path="home" element={<HomeBody />} />
+                        <Route path="findProfile" element={<FindProfile />} />
+                        <Route path="githubProfile" element={<GithubProfile />} />
+                        <Route path="*" element={<NotFound />} />
+                    </Route>
+                </Routes>
+            </BrowserRouter>
         </>
     );
 }
